f971cbaa1ba73840367f87103540264765d76b3e
patrickposch <patrick.posch@edu.campus02.at>
2018-04-07 12:02:26 +0200

    added function to get products from db based on a search string

--- geizhammer_project/src/model/ProdukteList.java
+++ geizhammer_project/src/model/ProdukteList.java
@@ -9,6 +9,8 @@ import java.util.LinkedList;
 public class ProdukteList {
 
 	LinkedList<Produkt> produkte = new LinkedList<Produkt>();
+	String url = String.format(
+			"jdbc:sqlserver://geizhammer.database.windows.net:1433;database=geizhammerDB;user=Edmin@geizhammer;password=SQL16db_2018_req;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
 
 	public void add(Produkt p) {
 		produkte.add(p);
@@ -21,11 +23,18 @@ public class ProdukteList {
 	public LinkedList<Produkt> getProdukte() {
 		return produkte;
 	}
+	
+	public void printListTest()
+	{
+		System.out.println("PID  Bezeichnung  Packungsgr  PackungsEH  Kategorie  Preis  Baumarkt");
+		for(Produkt p:produkte)
+		{
+			System.out.println(p.getPid() + "     " + p.getBezeichnung() + "       " + p.getPackungsgr() + "   " + p.getPackungsart() + "   " + p.getKategorie()
+					+ "   " + p.getPreis() + "   " + p.getBaumarkt());
+		}
+	}
 
 	public LinkedList<Produkt> getProdukteByKategorie(int kategorie) {
-
-		String url = String.format(
-				"jdbc:sqlserver://geizhammer.database.windows.net:1433;database=geizhammerDB;user=Edmin@geizhammer;password=SQL16db_2018_req;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;");
 
 		Connection connection = null;
 
@@ -38,8 +47,6 @@ public class ProdukteList {
 			try (Statement statement = connection.createStatement();
 					ResultSet resultSet = statement.executeQuery(selectSql)) {
 
-				// Print results from select statement
-				System.out.println("Result of SQL Query:");
 				while (resultSet.next()) {
 					int id = Integer.parseInt(resultSet.getString("PID"));
 					String bezeichnung = resultSet.getString("Bezeichnung");
@@ -52,8 +59,7 @@ public class ProdukteList {
 					Produkt p = new Produkt(id, bezeichnung, packungsgr, packungsart, kat, preis, baumarkt);
 					produkte.add(p);
 
-					System.out.println(id + "  " + bezeichnung + "  " + packungsgr + "  " + packungsart + "  " + kat
-							+ "  " + preis + "  " + baumarkt);
+				
 				}
 				connection.close();
 			}
@@ -61,6 +67,47 @@ public class ProdukteList {
 			e.printStackTrace();
 		}
 		return produkte;
+	}
+	
+	public LinkedList<Produkt> getProdukteByName(String search) {
+
+		Connection connection = null;
+
+		try {
+			connection = DriverManager.getConnection(url);
+
+			// Create and execute a SELECT SQL statement.
+			String selectSql = "SELECT * FROM tbl_Produkte where bezeichnung like '"+search+"%'";
+
+			try (Statement statement = connection.createStatement();
+					ResultSet resultSet = statement.executeQuery(selectSql)) {
+
+				while (resultSet.next()) {
+					int id = Integer.parseInt(resultSet.getString("PID"));
+					String bezeichnung = resultSet.getString("Bezeichnung");
+					int packungsgr = Integer.parseInt(resultSet.getString("Packungsgroeﬂe"));
+					int packungsart = Integer.parseInt(resultSet.getString("FKPack"));
+					int kat = Integer.parseInt(resultSet.getString("FKCat"));
+					float preis = Float.parseFloat(resultSet.getString("Preis"));
+					int baumarkt = Integer.parseInt(resultSet.getString("FKbaum"));
+
+					Produkt p = new Produkt(id, bezeichnung, packungsgr, packungsart, kat, preis, baumarkt);
+					produkte.add(p);
+
+				
+				}
+				connection.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return produkte;
+	}
+	
+	
+	public void pushSQLStatement(String sql)
+	{
+		
 	}
 
 }
